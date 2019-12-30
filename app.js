bac6a718e97ef9a64ca7debe0ac123168a43979e
Yesdoing <fkrmtleo12@gmail.com>
2019-12-30 18:21:40 +0900

    feat: add image download

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("controls__color");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = '#2c2c2c';
 const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -63,12 +66,25 @@ function handleCanvasClick() {
   }
 }
 
+function handleCM(event) {
+  event.preventDefault();
+}
+
+function handleSaveClick(event) {
+  const image = canvas.toDataURL("image/png");
+  const link = document.createElement('a');
+  link.href = image;
+  link.download = "PaintJS[🎉].jpeg";
+  link.click();
+}
+
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseEnter);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
   canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleCM);
 }
 
 
@@ -85,4 +101,8 @@ if (range) {
 
 if(mode) {
   mode.addEventListener("click", handleModeClick);
+}
+
+if(saveBtn) {
+  saveBtn.addEventListener('click', handleSaveClick);
 }
