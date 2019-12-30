0f49cbb6b0628122edadf958bccca79003d3d624
Yesdoing <fkrmtleo12@gmail.com>
2019-12-30 16:50:19 +0900

    feat: add Change color

---
+++ app.js
@@ -0,0 +1,47 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("controls__color");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = `#2c2c2c`;
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseEnter(event) {
+  const { offsetX, offsetY } = event;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(offsetX, offsetY);
+  } else {
+    ctx.lineTo(offsetX, offsetY);
+    ctx.stroke();
+  }
+}
+
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseEnter);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColorClick)
+);
